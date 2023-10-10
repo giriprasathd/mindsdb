0df217c2fca88bd3b09355724367d9f7c4ea45f7
GiriPrasath.D <hapisnake@gmail.com>
2023-10-10 13:05:52 +0530

    Update __about__.py

--- mindsdb/integrations/handlers/predictHQ_handler/__about__.py
+++ mindsdb/integrations/handlers/predictHQ_handler/__about__.py
@@ -1,1 +1,9 @@
-
+__title__ = 'MindsDB Predict handler'
+__package_name__ = 'mindsdb_Predict_handler'
+__version__ = '0.0.1'
+__description__ = "MindsDB handler for PredictHQ"
+__author__ = 'MindsDB Inc'
+__github__ = 'https://github.com/mindsdb/mindsdb'
+__pypi__ = 'https://pypi.org/project/mindsdb/'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyright 2023- mindsdb'
